e2657596d70b5d05b06d1288a88b7f5e9a84b6fc
Yonatan-Ga <harduf@gmail.com>
2021-07-24 17:51:46 +0300

    adherance choise working

---
+++ dashboard/src/components/drugs/MedAdherance.js
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import { RadialChart } from "react-vis";
+import { Card, Dropdown } from "react-bootstrap";
+
+function MedAdherancePage() {
+  const [adheranceTime, setAdheranceTime] = useState("m");
+  const [medicineChosen, setMedicineChosen] = useState("Choose Medicine:");
+  const [chosenMedObj, setChosenMedObj] = useState({
+    medicine_id: "",
+    pic: "",
+    m: 1,
+    w: 1,
+    d: 1,
+  });
+
+
+  function AdheranceTimeHandler(e) {
+    console.log(e);
+    setAdheranceTime(e);
+  };
+
+
+  const legend = {
+    m: "Monthly",
+    w: "Weekly",
+    d: "Daily"
+  };
+
+  const medUserAdherance = [
+    {
+      medicine_id: "Ventolin",
+      pic: "https://www.camoni.co.il/webfiles/images/cache/847X847/far/webfiles/camoni/cms_files/blogs/1985/posts/30286/105182.jpg",
+      m: 0.9,
+      w: 0.8,
+      d: 1,
+    },
+    {
+      medicine_id: "Optalgin",
+      pic: "https://superpharmstorage.blob.core.windows.net/hybris/products/desktop/medium/7290000801834.jpg",
+      m: 0.2,
+      w: 0.1,
+      d: 0.35,
+    },
+    {
+      medicine_id: "Acamol",
+      pic: "https://superpharmstorage.blob.core.windows.net/hybris/products/desktop/medium/7290000803234.jpg",
+      m: 0.67,
+      w: 0.71,
+      d: 0.8,
+    },
+    
+  ];
+
+//   const chosenMedObj = medUserAdherance.filter(x => x.medicine_id === "");
+
+
+
+function medUserAdheranceHandler (e) {
+    setChosenMedObj(medUserAdherance.filter(x => x.medicine_id === e))
+    console.log(chosenMedObj);
+    setMedicineChosen(e);
+    
+    
+};
+
+
+  const medUserAdheranceData = [
+    
+    {
+      angle: chosenMedObj[0][adheranceTime] * 100,
+      radius: 10,
+      label: "Taken: " + Math.round(chosenMedObj[0][adheranceTime] * 100) + "%",
+    },
+    {
+      angle: (1 - chosenMedObj[0][adheranceTime]) * 100,
+      radius: 10,
+      label: "Dismissed: " + Math.round((1 - chosenMedObj[0][adheranceTime]) * 100) + "%",
+    },
+  ];
+
+  return (
+        <div>
+
+          {/* Specific med adherance */}
+            <Card style={{ width: "18rem" }}>
+              <Card.Img
+                variant="top"
+                src=
+                {(medicineChosen === "Choose Medicine:") ? 
+                        "https://6lli539m39y3hpkelqsm3c2fg-wpengine.netdna-ssl.com/wp-content/uploads/2020/06/shutterstock_pills_medication-675x380.jpg" :
+                        chosenMedObj[0].pic}
+              />
+              <Card.Body>
+              <Dropdown onSelect={medUserAdheranceHandler}>
+                  <Dropdown.Toggle variant="primary" id="med-chooser">
+                    {medicineChosen}
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item eventKey="Acamol">Acamol</Dropdown.Item>
+                    <Dropdown.Item eventKey="Optalgin">Optalgin</Dropdown.Item>
+                    <Dropdown.Item eventKey="Ventolin">Ventolin</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+
+                <h2>{(medicineChosen === "Choose Medicine:") ? 
+                        "" :
+                        legend[adheranceTime] + ": " + chosenMedObj[0][adheranceTime] * 100 + "%"}</h2>
+                {(medicineChosen === "Choose Medicine:") ? 
+                  <img src="https://w7.pngwing.com/pngs/812/540/png-transparent-question-mark-graphy-thinking-man-thumbnail.png" alt="?" width="200"/> :
+                <RadialChart
+                  height={250}
+                  width={250}
+                  data={medUserAdheranceData}
+                  showLabels
+                />}
+
+                <Card.Title>Over All User Adherance</Card.Title>
+                <Card.Text>
+                  Adherance of all drugs together for a chosen time.
+                </Card.Text>
+                <Dropdown onSelect={AdheranceTimeHandler}>
+                  <Dropdown.Toggle variant="success" id="med-time-chooser">
+                    Adherance Time
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item eventKey="m">Monthly</Dropdown.Item>
+                    <Dropdown.Item eventKey="w">Weekly</Dropdown.Item>
+                    <Dropdown.Item eventKey="d">Daily</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Card.Body>
+            </Card>
+        </div>
+  );
+}
+
+export default MedAdherancePage;
--- dashboard/src/pages/Adherance.js
+++ dashboard/src/pages/Adherance.js
@@ -1,86 +1,89 @@
+import React, { useState } from "react";
 import { RadialChart } from "react-vis";
-import { Col, Container, Row, Card, Button } from "react-bootstrap";
+import { Col, Container, Row, Card, Dropdown } from "react-bootstrap";
+import MedAdherancePage from "../components/drugs/MedAdherance";
 
 function AdherancePage() {
-  const allData = [
-    { angle: 1, radius: 10, innerRadius: 7, label: "Dismissed" },
+  const [adheranceTime, setAdheranceTime] = useState("m");
+
+  function AdheranceTimeHandler(e) {
+    console.log(e);
+    setAdheranceTime(e);
+  };
+
+  
+  const userAdherance = {
+    month: "August",
+    m: 0.6,
+    w: 0.71,
+    d: 0.8,
+  };
+  
+  const legend = {
+    m: "Monthly",
+    w: "Weekly",
+    d: "Daily"
+  };
+  const userAdheranceData = [
     {
-      angle: 2,
-      label: "Snoozed",
+      angle: userAdherance[adheranceTime] * 100,
       radius: 10,
-      innerRadius: 7,
+      label: "Taken: " + Math.round(userAdherance[adheranceTime] * 100) + "%",
     },
-    { angle: 5, radius: 10, innerRadius: 7, label: "Taken" },
     {
-      angle: 5,
+      angle: (1 - userAdherance[adheranceTime]) * 100,
       radius: 10,
-      innerRadius: 7,
-      label: "Skipped",
+      label: "Dismissed: " + Math.round((1 - userAdherance[adheranceTime]) * 100) + "%",
     },
   ];
 
-  const acamolData = [
-    { angle: 1, radius: 10, innerRadius: 7, label: "Dismissed" },
-    {
-      angle: 2,
-      label: "Snoozed",
-      radius: 10,
-      innerRadius: 7,
-    },
-    { angle: 3, radius: 10, innerRadius: 7, label: "Taken" },
-    {
-      angle: 10,
-      radius: 10,
-      innerRadius: 7,
-      label: "Skipped",
-    },
-  ];
-
-  return (
+    return (
     <div>
       <h1>My Adherance</h1>
 
       <Container>
         <Row>
+
+            {/* All meds adherance */}
+
           <Col>
-            <Card style={{ width: "18rem", height: "38rem" }}>
+            <Card style={{ width: "18rem" }}>
               <Card.Img
                 variant="top"
                 src="https://6lli539m39y3hpkelqsm3c2fg-wpengine.netdna-ssl.com/wp-content/uploads/2020/06/shutterstock_pills_medication-675x380.jpg"
               />
               <Card.Body>
-                  <RadialChart
-                    height={250}
-                    width={250}
-                    data={allData}
-                    showLabels
-                  />
+                <h2>{legend[adheranceTime] + ": " + userAdherance[adheranceTime] * 100 + "%"}</h2>
+                <RadialChart
+                  height={250}
+                  width={250}
+                  data={userAdheranceData}
+                  showLabels
+                />
 
-                <Card.Title>Over All</Card.Title>
-                <Card.Text>Summery of all drugs in the system.</Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Card.Title>Over All User Adherance</Card.Title>
+                <Card.Text>
+                  Adherance of all drugs together for a chosen time.
+                </Card.Text>
+                <Dropdown onSelect={AdheranceTimeHandler}>
+                  <Dropdown.Toggle variant="success" id="user-time-chooser">
+                    Adherance Time
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item eventKey="m">Monthly</Dropdown.Item>
+                    <Dropdown.Item eventKey="w">Weekly</Dropdown.Item>
+                    <Dropdown.Item eventKey="d">Daily</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
               </Card.Body>
             </Card>
           </Col>
-          <Col>
-          <Card style={{ width: "18rem", height: "38rem" }}>
-              <Card.Img
-                variant="top"
-                src="https://superpharmstorage.blob.core.windows.net/hybris/products/desktop/medium/7290000803234.jpg"
-              />
-              <Card.Body>
-                  <RadialChart
-                    height={250}
-                    width={250}
-                    data={acamolData}
-                    showLabels
-                  />
 
-                <Card.Title>Acamol</Card.Title>
-                <Card.Text>Usualy used for headache or mild other types of pain.</Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
+          {/* Specific med adherance */}
+
+          <Col>
+            <MedAdherancePage />
           </Col>
         </Row>
       </Container>
